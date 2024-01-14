763b43a2223da466f19f2198ce21f22526e0e306
CamarataM <CamarataM@outlook.com>
2024-01-13 17:27:46 -0800

    Initial Commit

---
+++ scripts/GRollInitiative.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class GRollInitiative : Control {
+	[Export]
+	public Tree tree;
+
+	public override void _Ready() {
+	}
+
+	public override void _Process(double delta) {
+	}
+}
