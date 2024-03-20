a1a75da28fe081f70dbe400d0d2fd50e045298a6
CamarataM <CamarataM@outlook.com>
2024-03-20 16:19:50 -0700

    Implement spell slots

--- scripts/CreatureControl.cs
+++ scripts/CreatureControl.cs
@@ -64,6 +64,17 @@ public partial class CreatureControl : ResizableHContainer {
 		}
 	}
 
+	private Godot.Collections.Array<bool> _EnabledSpellSlots = new Godot.Collections.Array<bool>();
+	[Export]
+	public Godot.Collections.Array<bool> EnabledSpellSlots {
+		get => _EnabledSpellSlots;
+		set {
+			_EnabledSpellSlots = value;
+
+			Render(CreatureProperty.SPELL_SLOTS);
+		}
+	}
+
 	// Need a default constructor for Godot to be able to make the Node.
 	public CreatureControl() {
 		this.ColumnCount = 5;
@@ -135,6 +146,82 @@ public partial class CreatureControl : ResizableHContainer {
 
 								GRollInitiative.StaticEditPropertyCellConfirmationDialog.AddChild(spinBox);
 								break;
+							case CreatureProperty.SPELL_SLOTS:
+								// Create a parent container to contain the sub-container which will hold the spell slot controls, the add button, and the spinbox.
+								var spellSlotsControlVBoxContainer = new VBoxContainer();
+								// Create a container which will contain the spell slot controls.
+								var spellSlotsCheckButtonVBoxContainer = new VBoxContainer();
+
+								var addButtonAmountSpinbox = new SpinBox();
+								addButtonAmountSpinbox.Value = 9;
+
+								HBoxContainer CreateSpellSlotContainer() {
+									// Create a HBoxContainer which will contain the SwitchButton and the delete button for the spell slot.
+									var spellSlotsEnabledContainer = new HBoxContainer();
+									spellSlotsEnabledContainer.AddChild(new CheckButton());
+
+									var deleteButton = new Button();
+									deleteButton.Text = "X";
+									deleteButton.Pressed += () => {
+										deleteButton.GetParent().QueueFree();
+										deleteButton.GetParent().GetParent().RemoveChild(deleteButton.GetParent());
+
+										RefreshIndexes();
+									};
+
+									spellSlotsEnabledContainer.AddChild(deleteButton);
+
+									spellSlotsCheckButtonVBoxContainer.AddChild(spellSlotsEnabledContainer);
+
+									return spellSlotsEnabledContainer;
+								}
+
+								// Set the indexes of the spell slot CheckButtons based on their index in the children list of their parent VBoxContainer.
+								void RefreshIndexes() {
+									int spellSlotIndex = 0;
+									foreach (var child in spellSlotsCheckButtonVBoxContainer.GetChildren()) {
+										if (child is HBoxContainer hBoxContainer) {
+											hBoxContainer.GetChild<CheckButton>(0).Text = "Slot " + (spellSlotIndex + 1);
+
+											spellSlotIndex += 1;
+										}
+									}
+								}
+
+								// Add any existing spell slots for the current creature.
+								foreach (var enabledSlot in this.EnabledSpellSlots) {
+									var spellSlotContainer = CreateSpellSlotContainer();
+									spellSlotContainer.GetChild<CheckButton>(0).ButtonPressed = enabledSlot;
+								}
+								RefreshIndexes();
+
+								var addButton = new Button();
+								addButton.Text = "Add";
+								addButton.Pressed += () => {
+									for (int amountToAdd = 0; amountToAdd < addButtonAmountSpinbox.Value; amountToAdd++) {
+										CreateSpellSlotContainer();
+									}
+
+									RefreshIndexes();
+								};
+
+								var deleteAllButton = new Button();
+								deleteAllButton.Text = "Delete All";
+								deleteAllButton.Pressed += () => {
+									foreach (var child in spellSlotsCheckButtonVBoxContainer.GetChildren()) {
+										if (child is HBoxContainer hBoxContainer) {
+											hBoxContainer.QueueFree();
+										}
+									}
+								};
+
+								spellSlotsControlVBoxContainer.AddChild(addButton);
+								spellSlotsControlVBoxContainer.AddChild(addButtonAmountSpinbox);
+								spellSlotsControlVBoxContainer.AddChild(deleteAllButton);
+								spellSlotsControlVBoxContainer.AddChild(spellSlotsCheckButtonVBoxContainer);
+
+								GRollInitiative.StaticEditPropertyCellConfirmationDialog.AddChild(spellSlotsControlVBoxContainer);
+								break;
 							default:
 								GD.PrintErr("Unhandled CreatureProperty '" + creatureProperty + "'");
 								break;
@@ -160,6 +247,32 @@ public partial class CreatureControl : ResizableHContainer {
 								case CreatureProperty.INITIATIVE:
 									this.Initiative = (int) GRollInitiative.StaticEditPropertyCellConfirmationDialog.GetChild<SpinBox>(0).Value;
 									break;
+								case CreatureProperty.SPELL_SLOTS:
+									this.EnabledSpellSlots.Clear();
+
+									// Iterate each spell slot container, checking if it is checked or not, setting that index value into the EnabledSpellSlots Array.
+									foreach (var child in GRollInitiative.StaticEditPropertyCellConfirmationDialog.GetChild<VBoxContainer>(0).GetChildren()) {
+										if (child is VBoxContainer) {
+											int spellSlotIndex = 0;
+											foreach (var spellSlotParentContainerChild in child.GetChildren()) {
+												if (spellSlotParentContainerChild is HBoxContainer hBoxContainer) {
+													var checkButton = hBoxContainer.GetChild<CheckButton>(0);
+
+													if (spellSlotIndex >= this.EnabledSpellSlots.Count) {
+														this.EnabledSpellSlots.Add(checkButton.ButtonPressed);
+													} else {
+														this.EnabledSpellSlots[spellSlotIndex] = checkButton.ButtonPressed;
+													}
+
+													spellSlotIndex += 1;
+												}
+											}
+										}
+									}
+
+									Render();
+
+									break;
 								default:
 									GD.PrintErr("Unhandled CreatureProperty '" + creatureProperty + "'");
 									break;
@@ -167,6 +280,10 @@ public partial class CreatureControl : ResizableHContainer {
 						};
 
 						GRollInitiative.StaticEditPropertyCellConfirmationDialog.Position = (Vector2I) (GetWindow().Position + inputEventMouseButton.GlobalPosition - (GRollInitiative.StaticEditPropertyCellConfirmationDialog.Size / 2) + new Vector2(0, 30));
+
+						// Set the size of the confirmation dialog equal to the minimum size required plus an offset.
+						GRollInitiative.StaticEditPropertyCellConfirmationDialog.Size = (Vector2I) GRollInitiative.StaticEditPropertyCellConfirmationDialog.GetContentsMinimumSize() + new Vector2I(100, 10);
+
 						GRollInitiative.StaticEditPropertyCellConfirmationDialog.Show();
 					}
 				}
@@ -221,17 +338,32 @@ public partial class CreatureControl : ResizableHContainer {
 						break;
 					case CreatureProperty.SPELL_SLOTS:
 						var spellSlotsHFlowContainer = new HFlowContainer();
+						bool haveEnabledSpellSlots = false;
 
-						for (int i = 0; i < 9; i++) {
+						// Set the spell slot text and update the visibility based on whether the spell slot is enabled or not.
+						for (int i = 0; i < EnabledSpellSlots.Count; i++) {
 							var spellSlotHBoxContainer = new HBoxContainer();
 
-							// spellSlotHBoxContainer.AddChild(new Label() {
-							// 	Text = (i + 1).ToString(),
-							// });
+							spellSlotHBoxContainer.AddChild(new Label() {
+								Text = (i + 1).ToString(),
+							});
 
 							spellSlotHBoxContainer.AddChild(new SpinBox());
 
+							// Set the visibility of the spell slot container to be equal to whether the spell slot is enabled or not.
+							spellSlotHBoxContainer.Visible = EnabledSpellSlots[i];
+							if (spellSlotHBoxContainer.Visible) {
+								haveEnabledSpellSlots = true;
+							}
+
 							spellSlotsHFlowContainer.AddChild(spellSlotHBoxContainer);
+						}
+
+						// Check if we have any visible children. If not, add a label which states that we have no spell slots in the container.
+						if (!haveEnabledSpellSlots) {
+							spellSlotsHFlowContainer.AddChild(new Label() {
+								Text = "No Spell Slots",
+							});
 						}
 
 						panelContainer.AddChild(spellSlotsHFlowContainer);
